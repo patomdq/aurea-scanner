c6cb38b8c5cf3803684c7d45e916ede2b3916fed
patomdq <mdq.patricio@gmail.com>
2026-05-14 13:51:15 +0200

    Add files via upload

---
+++ scan.js
@@ -0,0 +1,23 @@
+const FMP_KEY = 'XPfNnraUVCbn6rnjJFUMkaJXrXm2K4JQ';
+const FMP = 'https://financialmodelingprep.com/stable';
+
+async function fmpFetch(endpoint) {
+  const sep = endpoint.includes('?') ? '&' : '?';
+  const res = await fetch(`${FMP}${endpoint}${sep}apikey=${FMP_KEY}`);
+  if (!res.ok) throw new Error(`FMP ${res.status}`);
+  return res.json();
+}
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const [gainers, losers, actives] = await Promise.all([
+      fmpFetch('/gainers'),
+      fmpFetch('/losers'),
+      fmpFetch('/actives')
+    ]);
+    res.status(200).json({ gainers, losers, actives });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
