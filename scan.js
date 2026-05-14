9023264c93f275780dde8961cfe7839f84cd1877
patomdq <mdq.patricio@gmail.com>
2026-05-14 14:01:56 +0200

    Update FMP API endpoint and fetch methods

--- scan.js
+++ scan.js
@@ -1,5 +1,5 @@
 const FMP_KEY = 'XPfNnraUVCbn6rnjJFUMkaJXrXm2K4JQ';
-const FMP = 'https://financialmodelingprep.com/stable';
+const FMP = 'https://financialmodelingprep.com/api/v3';
 
 async function fmpFetch(endpoint) {
   const res = await fetch(`${FMP}${endpoint}&apikey=${FMP_KEY}`);
@@ -11,9 +11,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const [gainers, losers, actives] = await Promise.all([
-      fmpFetch('/biggest-gainers?'),
-      fmpFetch('/biggest-losers?'),
-      fmpFetch('/most-actives?')
+      fmpFetch('/gainers?'),
+      fmpFetch('/losers?'),
+      fmpFetch('/actives?')
     ]);
     res.status(200).json({ gainers, losers, actives });
   } catch (err) {
